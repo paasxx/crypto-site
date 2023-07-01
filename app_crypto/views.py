7a6fc370e8faeb2b29746730a79942bcb7e246e7
Pedro <paasxx@gmail.com>
2023-07-01 19:58:27 -0300

    Plot done

--- app_crypto/views.py
+++ app_crypto/views.py
@@ -124,30 +124,78 @@ def plot_html(df, title):
 
     #     eth_df.set_index('Date')
 
-    fig = go.Figure(
-        data=[
-            go.Candlestick(
-                x=df["date"],
-                open=df["open"],
-                high=df["high"],
-                low=df["low"],
-                close=df["close"],
-            )
-        ]
+    fig = make_subplots(
+        rows=2,
+        cols=1,
+        shared_xaxes=True,
+        vertical_spacing=0.07,
+        subplot_titles=("OHLC", "Volume"),
+        row_width=[0.2, 0.7],
+    )
+
+    # Plot OHLC on 1st row
+    fig.add_trace(
+        go.Candlestick(
+            x=df["date"],
+            open=df["open"],
+            high=df["high"],
+            low=df["low"],
+            close=df["close"],
+            showlegend=False,
+        ),
+        row=1,
+        col=1,
     )
 
     fig.update_layout(
-        xaxis_rangeslider_visible=True,
+        xaxis_rangeslider_visible=False,
         template="plotly_dark",
+        plot_bgcolor="rgba(0, 0, 0, 0.2)",
+        paper_bgcolor="rgba(0, 0, 0, 0.2)",
     )
 
     fig.update_layout(
-        title=title,
+        title=title.upper(),
         yaxis_title="Price",
         autosize=True,
         # width=500,
-        height=1000,
-        margin=dict(l=50, r=50, b=100, t=100, pad=4),
+        height=800,
+        margin=dict(l=50, r=50, b=100, t=100, pad=1),
+    )
+
+    fig.update_xaxes(
+        mirror=True,
+        ticks="outside",
+        showline=True,
+        linecolor="grey",
+        gridcolor="grey",
+        title_standoff=0,
+    )
+    fig.update_yaxes(
+        mirror=True,
+        ticks="outside",
+        showline=True,
+        linecolor="grey",
+        gridcolor="grey",
+        title_standoff=10,
+    )
+
+    # Bar trace for volumes on 2nd row without legend
+    fig.add_trace(go.Bar(x=df["date"], y=df["volume"], showlegend=False), row=2, col=1)
+
+    fig.update_layout(
+        annotations=[
+            go.layout.Annotation(
+                {
+                    "yshift": 10,
+                    "font": dict(
+                        # family="Courier New, monospace",
+                        size=18,
+                        # color="#ffffff"
+                    ),
+                }
+            )
+        ]
     )
 
     return fig.to_html()
