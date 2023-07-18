6ad22f6a720aa78b4d6b4e307b245af09d5d3a84
Pedro <paasxx@gmail.com>
2023-07-17 21:45:08 -0300

    implementing postgre

--- crypto_data/settings.py
+++ crypto_data/settings.py
@@ -81,11 +81,23 @@ WSGI_APPLICATION = "crypto_data.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         # "NAME": BASE_DIR / "db.sqlite3",
+#         "NAME": BASE_DIR / "crypto_real",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         # "NAME": BASE_DIR / "db.sqlite3",
-        "NAME": BASE_DIR / "crypto_real",
+        "NAME": "crypto_data",
+        "USER": "postgres",
+        "PASSWORD": "Darthmaul18*",
+        "PORT": "5432",
+        "HOST": "localhost",
     }
 }
 
